8b6d28afc9e78c4cc42387167e9f0439d4dbcc9a
Eyad156 <eyad1509@gmail.com>
2024-03-25 04:41:29 +0200

    Eyad Emad

---
+++ main/main.js
@@ -0,0 +1,8 @@
+var end_page = document.getElementById('end')
+end_page.style.backgroundColor = '#252543';
+end_page.style.marginTop = '150px'
+end_page.style.width = '1450px'
+end_page.style.height = '320px'
+console.warn('This website maked by @Eyad Emad')
+console.log("You get here at : ",Date())
+console.error("Sorry, You can't edit on this website right Now.")
